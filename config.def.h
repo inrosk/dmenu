993a40152d66b16f888424b69a9c33760695c206
karol <kr@inrosk.com>
2025-09-30 14:34:45 +0200

    My config

--- config.def.h
+++ config.def.h
@@ -1,23 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy  = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 1;                        /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy  = 1;                        /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+static int centered = 0;                      /* -c option; centers dmenu on screen */
+static int min_width = 948;                   /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"SFMono Nerd Font:size=8",
+	"monospace:size=8"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] =          { "#ebdbb2", "#1d2021" },
+	[SchemeSel] =           { "#fbf1c7", "#504945" },
+	[SchemeSelHighlight] =  { "#fabd2f", "#504945" },
+	[SchemeNormHighlight] = { "#fabd2f", "#1d2021" },
+	[SchemeOut] =           { "#0d1011", "#83a598" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 0;
